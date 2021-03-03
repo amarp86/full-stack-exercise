90e8e0378a292687a5c965c3284cd7e997017fab
Amar Patel <amarpatel@amars-air.myfiosgateway.com>
2021-03-03 12:26:10 -0500

    lunch time push and also form works

---
+++ client/src/screens/Add.jsx
@@ -0,0 +1,69 @@
+import Layout from "../components/shared/Layout";
+import { useState } from "react";
+import { createPost } from "../services/postsAxios";
+import { useHistory } from "react-router-dom";
+
+const Add = (props) => {
+  const history = useHistory();
+
+  const [post, setPost] = useState({
+    title: "",
+    content: "",
+    imgURL: "",
+    author: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createPost(post);
+    history.push("/");
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPost({
+      ...post,
+      [name]: value,
+    });
+  };
+
+  return (
+    <Layout>
+      <div className="add-div">
+        <form className="el-formo" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add-o Title-o Here-o"
+            value={post.title}
+            name="title"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Add-o Content-o Here-o"
+            value={post.content}
+            name="content"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Add-o Author-o Here-o"
+            value={post.author}
+            name="author"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Add-o Image-Link-o Here-o"
+            value={post.imgURL}
+            name="imgURL"
+            onChange={handleChange}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Add;
