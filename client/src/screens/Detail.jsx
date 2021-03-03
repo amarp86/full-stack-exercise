a8250a4b9606594009c8a0579a5e58297dd29567
Amar Patel <amarpatel@amars-air.myfiosgateway.com>
2021-03-03 13:51:14 -0500

    team got update to work

--- client/src/screens/Detail.jsx
+++ client/src/screens/Detail.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Layout from "../components/shared/Layout";
-
+import { Link } from "react-router-dom";
 import { getPost } from "../services/postsAxios";
 
 function Detail(props) {
