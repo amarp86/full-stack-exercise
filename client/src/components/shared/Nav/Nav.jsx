69e6728e25725c7b4090ccedc2b801b6c6a92244
Amar Patel <amarpatel@amars-air.myfiosgateway.com>
2021-03-03 15:20:33 -0500

    surge

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -4,7 +4,7 @@ function Nav(props) {
   return (
     <div className="nav-div">
       <p>Here is our Navo</p>
-      <Link to="/">Home Here</Link>
+      <Link to="/">Home</Link>
       <Link to="/add">Add</Link>
     </div>
   );
